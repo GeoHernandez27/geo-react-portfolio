b249229bdde523ecb5501b69bd77c7cd16778cb8
GeoHernandez27 <leopardo2@gmail.com>
2019-04-24 11:11:04 -0600

    created portfolio manager link and set up grid layout

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import PortfolioManager from "./pages/portfolio-manager";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
@@ -74,7 +75,9 @@ export default class App extends Component {
   }
 
   authorizedPages() {
-    return [<Route path="/blog" component={Blog} />];
+    return [
+      <Route path="/portfolio-manager" component={PortfolioManager} />
+    ];
   }
 
   render() {
@@ -86,8 +89,6 @@ export default class App extends Component {
             loggedInStatus={this.state.loggedInStatus} 
             handleSuccessfulLogout={this.handleSuccessfulLogout}
             />
-
-            <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
               <Route exact path="/" component={Home} />
@@ -104,10 +105,11 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
               {this.state.loggedInStatus === "LOGGED_IN"
                 ? this.authorizedPages()
                 : null}
-
+              
               <Route
                 exact
                 path="/portfolio/:slug"
---
+++ src/components/pages/portfolio-manager.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class PortfolioManager extends Component {
+    render() {
+        return (
+            <div className="portfolio-manager-wrapper">
+                <div className="left-column">
+                <h1>Portfolio Form...</h1>
+                </div>
+               <div className="right-column">
+               <h1>Portfolio sidebar...</h1>
+               </div>
+                
+            </div>
+        )
+    }
+}
+
+export default PortfolioManager;
