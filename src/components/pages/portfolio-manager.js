c8985749e5a7bdb1f9e2c19c1b7b3562b4fdd8eb
GeoHernandez27 <leopardo2@gmail.com>
2019-04-24 12:57:33 -0600

    created sidebar component with base styles

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,19 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
 class PortfolioManager extends Component {
-    render() {
-        return (
-            <div className="portfolio-manager-wrapper">
-                <div className="left-column">
-                <h1>Portfolio Form...</h1>
-                </div>
-               <div className="right-column">
-               <h1>Portfolio sidebar...</h1>
-               </div>
-                
-            </div>
-        )
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+  }
+
+  getPortfolioItems() {
+    axios.get("https://geohernandez.devcamp.space/portfolio/portfolio_items", { 
+      withCredentials: true
+    }).then(response => {
+      this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+      });
+    }).catch(error => {
+      console.log("error in getPortfolioItems", error);
+    })
+  }
+  
+
+  componentDidMount(){
+      this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio Form...</h1>
+        </div>
+        <div className="right-column">
+          <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PortfolioManager;
