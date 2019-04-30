7a4eef0c897b2cdfaff8acfe00faf09c44fec057
GeoHernandez27 <leopardo2@gmail.com>
2019-04-30 15:32:12 -0600

    Connected blog component to routing engine and blox index page

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-  const BlogItem = props => {
-    const {
-      id,
-      blog_status,
-      content,
-      title,
-      featured_image_url
-    } = props.blogItem;
-
+const BlogItem = props => {
+  const {
+    id,
+    blog_status,
+    content,
+    title,
+    featured_image_url
+  } = props.blogItem;
 
   return (
     <div>
-      <h1>{title}</h1>
+      <Link to={`/b/${id}`}>
+        <h1>{title}</h1>
+      </Link>
       <div>{content} </div>
     </div>
   );
