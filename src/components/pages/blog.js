f26ed598f2957dc2fcd348c615dd9f9f01685699
GeoHernandez27 <leopardo2@gmail.com>
2019-05-01 13:47:58 -0600

    Added styles to modal and close out functionality

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,12 +22,19 @@ class Blog extends Component {
     this.onScroll = this.onScroll.bind(this);
     window.addEventListener("scroll", this.onScroll, false);
     this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
-  
+
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
   handleNewBlogClick() {
     this.setState({
       blogModalIsOpen: true
-    })
+    });
   }
 
   onScroll() {
@@ -88,10 +95,13 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
-      <BlogModal modalIsOpen={this.state.blogModalIsOpen} />
-      <div className="new-blog-link">
-        <a onClick={this.handleNewBlogClick}>Open Modal</a>
-      </div>
+        <BlogModal
+          handleModalClose={this.handleModalClose}
+          modalIsOpen={this.state.blogModalIsOpen}
+        />
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal</a>
+        </div>
 
         <div className="content-container">{blogRecords}</div>
 
