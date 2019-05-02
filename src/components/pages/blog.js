5093fdd971f3c5af38cd5a58ab0fbcc79ab7df26
GeoHernandez27 <leopardo2@gmail.com>
2019-05-02 13:09:02 -0600

    fixed unmounted component warning bug

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -77,7 +77,7 @@ class Blog extends Component {
         }
       )
       .then(response => {
-        console.log("getting", response.data);
+        
         this.setState({
           blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
