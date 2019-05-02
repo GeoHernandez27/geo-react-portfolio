9b9ea86bcf0bf684e26ce256d04f7eed4ebece59
GeoHernandez27 <leopardo2@gmail.com>
2019-05-02 14:19:12 -0600

    added ability to upload featured images to blogs

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -23,6 +23,7 @@ export default class BlogForm extends Component {
     this.componentConfig = this.componentConfig.bind(this);
     this.djsConfig = this.djsConfig.bind(this);
     this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+    this.featuredImageRef = React.createRef();
   }
 
   componentConfig() {
@@ -30,7 +31,7 @@ export default class BlogForm extends Component {
       iconFiletypes: [".jpg", ".png"],
       showFiletypeIcon: true,
       postUrl: "https://httpbin.org/post"
-    }
+    };
   }
 
   djsConfig() {
@@ -57,6 +58,13 @@ export default class BlogForm extends Component {
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
     formData.append("portfolio_blog[content]", this.state.content);
 
+    if (this.state.featured_image) {
+      formData.append(
+        "portfolio_blog[featured_image]",
+        this.state.featured_image
+      );
+    }
+
     return formData;
   }
 
@@ -67,11 +75,17 @@ export default class BlogForm extends Component {
         this.buildForm(),
         { withCredentials: true }
       )
+
       .then(response => {
+        if (this.state.featured_image) {
+          this.featuredImageRef.current.dropzone.removeAllFiles();
+        }
+
         this.setState({
           title: "",
           blog_status: "",
-          content: ""
+          content: "",
+          featured_image: ""
         });
 
         this.props.handleSuccessfullFormSubmission(
@@ -121,6 +135,7 @@ export default class BlogForm extends Component {
 
         <div className="image-uploaders">
           <DropzoneComponent
+            ref={this.featuredImageRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleFeaturedImageDrop()}
