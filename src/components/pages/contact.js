8a331eb1879d0d59226715e776d5c7b875df31c1
GeoHernandez27 <leopardo2@gmail.com>
2019-05-03 12:02:36 -0600

    Built portfolio detail and rendered data on the screen

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,27 +1,10 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/testImage.jpg";
+
 
 export default function() {
   return (
-    <div className="content-page-wrapper ">
-      <div
-        className="left-column"
-        style={{
-          background: "url(" + profilePicture + ") no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      >
-        image
-      </div>
-      <div className="right-column">
-        <h1>About text goes here...</h1> Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Aliquam auctor sapien eget iaculis dignissim. Morbi sed
-        metus ullamcorper, euismod elit in, tincidunt nisl. Nulla malesuada est
-        arcu, nec bibendum lacus viverra ut. Suspendisse potenti. Aenean felis
-        nulla, porta eget felis id, sagittis efficitur ante. Sed bibendum magna
-        sed risus tempus scelerisque. 
-      </div>
+    <div >
+      Contact
     </div>
   );
 }
--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
+export default class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
 
-export default function(props) {
-    return(
-        <div>
-            <h2>Portfolio Detail for {props.match.params.slug} </h2>
-        </div>
-    )
+    this.state = {
+      portfolioItem: {}
+    };
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://geohernandez.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        });
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getPortfolioItem();
+  }
+
+  render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem;
+    return (
+      <div>
+        <h2>{name} </h2>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
